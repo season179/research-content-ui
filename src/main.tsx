ac681e5f53366b89fdb034121b61273a32e3ef0e
Season Saw <season.saw@paywatchglobal.com>
2024-12-20 11:26:59 +0800

    Added PostHog

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { PostHogProvider } from 'posthog-js/react'
 import { App } from "./App";
 import "./index.css";
+
+const options = {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+}
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 createRoot(rootElement).render(
     <React.StrictMode>
-        <App />
+        <PostHogProvider 
+            apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+            options={options}
+        >
+            <App />
+        </PostHogProvider>
     </React.StrictMode>
 );
